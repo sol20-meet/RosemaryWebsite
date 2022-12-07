fb4e9a63bfe1bd548a661140468d0364cd936bc5
Sol Diab <soldiab12@gmail.com>
2022-12-07 15:05:56 +0200

    Changed File Structure

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-#Rosemary Reservations
-A website which was made to make the whole reservations procces easier ya 3umreeeeeee
+# Vercel flask
+A demo Flask application running on Vercel
 
-![alt text](https://i.giphy.com/media/KOOY2YuGf4czGnf69p/giphy.webp)
+# Deployment
+
+[![Deploy to Vercel](https://camo.githubusercontent.com/f209ca5cc3af7dd930b6bfc55b3d7b6a5fde1aff/68747470733a2f2f76657263656c2e636f6d2f627574746f6e)](https://vercel.com/import/project?template=https://github.com/caibingcheng/vercel-flask)
--- api/app.py
+++ api/app.py
@@ -49,17 +49,6 @@ def add():
 		return render_template('add.html' , data=[{'name':'Roza'}, {'name':'Hanan'}, {'name':'Sol'}, {'name':'Ameer'}, {'name':'Hamoodi'}, {'name':'Tarek'}, {'name':'Elias'}, {'name':'Waseem'}, {'name':'Ghassan'}, {'name':'Hisham'}, {'name':'William'}, {'name':'Jwan'} , {'name':'Tala'} , {'name':'Rosol'}] , data1=[{'time':'12:00'},{'time':'12:15'},{'time':'12:30'},{'time':'12:45'},{'time':'13:00'},{'time':'13:15'},{'time':'13:30'},{'time':'13:45'},{'time':'14:00'},{'time':'14:15'},{'time':'14:30'},{'time':'14:45'},{'time':'15:00'},{'time':'15:15'},{'time':'15:30'},{'time':'15:45'},{'time':'16:00'},{'time':'16:15'},{'time':'16:30'},{'time':'16:45'},{'time':'17:00'},{'time':'17:15'},{'time':'17:30'},{'time':'17:45'},{'time':'18:00'},{'time':'18:15'},{'time':'18:30'},{'time':'18:45'},{'time':'19:00'},{'time':'19:15'},{'time':'19:30'},{'time':'19:45'},{'time':'20:00'},{'time':'20:15'},{'time':'20:30'},{'time':'20:45'},{'time':'21:00'},{'time':'21:15'},{'time':'21:30'},{'time':'21:45'},{'time':'22:00'},{'time':'22:00 +'}])
 	else:
 		print("Creating Reservation Object")
-		# reserveName = request.form['reserveName']
-		# fullName = request.form['fullName']
-		# reserveTime = request.form.get('comp_select1')
-		# reserveDay = request.form['reserveDay']
-		# numOfpeople = request.form.get('comp_select3')
-		# location = request.form.get('comp_select2')
-		# phone_num = request.form['phoneNum']
-		# waiterName = request.form.get('comp_select')
-		# isEating = request.form['isEating']
-		# closedMenu = request.form['closedMenu']
-		# notes = request.form['notes']
 
 		reserveObj = {
 		"reserveName" : request.form['reserveName'],
@@ -81,6 +70,7 @@ def add():
 		print("Done!")
 		return redirect('/home')
 
+
 @app.route('/view' , methods=['GET' , 'POST'])
 def view():
 	if request.method == 'GET':
@@ -93,9 +83,6 @@ def view():
 		objId = request.form['deleteBtn']
 		db.child("reservations").child(objId).remove()
 		return redirect("/view")
-
-
-
 
 
 if __name__ == '__main__':
